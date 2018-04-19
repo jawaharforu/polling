4dac0353bf34b007d8645d8a8ad8e1023fc38143
Jawahar <jawaharforu@gmail.com>
2018-04-19 12:39:22 +0530

    new

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -4415,10 +4415,13 @@ var PollService = /** @class */ (function () {
     };
     // get ip detail
     PollService.prototype.getIpDetail = function (ip) {
-        var headers = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["Headers"]({
-            'Access-Control-Allow-Origin': '*'
-        });
-        return this.http.get('//api.ip2location.com/?ip=' + ip + '&key=0FF79BE7E0&package=WS3', { headers: headers })
+        // const headers = new Headers({
+        //   'Access-Control-Allow-Origin': '*',
+        //   'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        //   'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+        //   'Access-Control-Allow-Credentials': true
+        // });
+        return this.http.get('//api.ip2location.com/?ip=' + ip + '&key=0FF79BE7E0&package=WS3')
             .map(function (response) { return response; });
     };
     PollService.prototype.upload = function (fileToUpload) {
