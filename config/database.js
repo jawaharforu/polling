3853191e20a5a1de88cc6f0b81aa29b9f4ec6886
Jawahar <jawaharforu@gmail.com>
2018-04-30 09:25:06 +0530

    db change

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-    database: 'mongodb://jawaharzolipe:jawaharzolipe@ds127899.mlab.com:27899/polling', //prod
+    //database: 'mongodb://jawaharzolipe:jawaharzolipe@ds127899.mlab.com:27899/polling', //prod
     //database: 'mongodb://localhost/polling',
+    database: 'mongodb://127.0.0.1:27017/polling',
     secret: 'yoursecret'
 }  
